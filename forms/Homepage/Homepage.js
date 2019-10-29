5d726fd48449dbfb6bb175cea2212ab55efc72c1
Shuyi Chen <suechenusa@gmail.com>
2019-10-28 19:52:28 -0500

    Finish All part
    Finish All part

---
+++ forms/Homepage/Homepage.js
@@ -0,0 +1,20 @@
+
+CustomerSelect.onclick=function(){
+  ChangeForm(customerSelect)
+}
+
+CustomerDeleting.onclick=function(){
+  ChangeForm(customerDeleting)
+}
+
+CustomerAdding.onclick=function(){
+  ChangeForm(customerAdding)
+}
+
+CustomerUpdating.onclick=function(){
+  ChangeForm(customerUpdating)
+}
+
+Button1.onclick=function(){
+  ChangeForm(extraXP)
+}
